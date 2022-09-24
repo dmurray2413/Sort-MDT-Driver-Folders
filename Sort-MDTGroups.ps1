e45b6254e94803103df1c4e34b0f9ae663485063
Dan Murray <dmurray@murdan.net>
2022-09-24 12:12:00 -0400

    Updated to allow group xml selection
    Updated to allow group xml selection

---
+++ Sort-MDTGroups.ps1
@@ -0,0 +1,121 @@
+﻿#Standard GPLv3 License
+#This script sorts the MDT driver folders into alphabetical order.
+#If you're overwriting the path and running this script please be sure to BACKUP your original DriverGroups.xml file.
+#Please feel free to tweak this script to suit your needs.
+#Below there is an exclude group list.  I'd recommend putting your top level folders in this group. (Default and Hidden are already hard coded in)
+#______________________________________________________________________________________________________
+#Author: Mike Kisiel 
+#Contributors:
+# D.Murray
+#Latest Update: 09/24/2022 12:01:21 D Murray
+#Patch Notes:
+#v1.0.2 - Added File Selection vs HardCoded File Name.
+#v1.0.1 - Added file backup feature.
+#v1.0 - Release version
+#______________________________________________________________________________________________________
+function Get-File {   
+    [CmdletBinding()]
+    [Alias("DMGet-File")]
+    Param(
+        $initialDirectory,
+        $Title,
+        $Filter = “All Files (*.*)|*.*|Text files (*.txt)|*.txt"
+        )
+
+    [void] [System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    if ($initialDirectory) { $OpenFileDialog.initialDirectory = $initialDirectory }
+    $OpenFileDialog.Title = if($Title -eq $null){"Select File"}else{$Title}
+    $OpenFileDialog.Multiselect=$true  #does not work
+    $OpenFileDialog.filter = if($Filter -ne $null){$Filter}
+    [void] $OpenFileDialog.ShowDialog()
+    return $OpenFileDialog.FileName
+} # Function Get-File($initialDirectory)
+
+#Set the path to the DriverGroups.XML file you wish to edit.
+#$FileToEdit = "C:\DeploymentShare$\Control\DriverGroups.xml"
+$FileToEdit = Get-File -Filter "Group Files (*Groups.xml)|*Groups.xml" #v1.0.2
+
+#Set the export path where you want the new file to go.
+#$exportPath = "C:\DeploymentShare$\Control\DriverGroups.xml"
+$exportPath = $($FileToEdit) #v1.0.2
+
+##Set back up options - A file will be created with the date appended to the file name in the directory you set.
+#Turn the back up file feature On/Off (Off by default)
+$BackupFileOn = $false
+#Set the folder where you want the backups to go
+#$BackupFolderPath = 'C:\DeploymentShare$\Control\DriverGroups\'
+$BackupFolderPath = "$((split-path $FileToEdit),"Backups" -join '\')\"  #v1.0.2
+
+#Exits the program if it is unable to read/find the file.
+Try{[xml]$xml = get-content $FileToEdit -ErrorAction Stop}
+Catch{
+Write-Host "Could not access path $FileToEdit. Check your path/access rights." -ForegroundColor Yellow
+Exit
+}
+
+#Backup original file
+If ($BackupFileOn -eq $true){
+    #Copy the original file to the specified folder and append a data value to its name. 
+     If (Test-Path -Path $BackupFolderPath){
+        #Copys the file
+        Try{
+            Copy-Item $FileToEdit `
+                -Destination $BackupFolderPath\DriverGroups$((Get-Date).ToString('MM-dd-yyyy')).xml -Force -ErrorAction Stop}
+        catch{Write-Host "Could not access path $BackupFolderPath. Check your path/access rights." -ForegroundColor Yellow}
+    }Else{ 
+        #Creates directory if it doesn't exist then copys the file.
+        Try{New-Item -ItemType 'Directory' -Path $BackupFolderPath -ErrorAction Stop | Out-Null
+        Copy-Item $FileToEdit -Destination $BackupFolderPath\DriverGroups$((Get-Date).ToString('MM-dd-yyyy')).xml -Force -ErrorAction Stop}
+        Catch{Write-Host "Could not move file to $BackupFolderPath"}
+    }
+}
+
+
+#Create the list objects we'll be using
+$GroupList = New-Object System.Collections.Generic.List[System.Object]
+$ReadOnlyList = New-Object System.Collections.Generic.List[System.Object]
+
+#Create groups that are at the top level you do not want to remove or have explicitly sorted
+#Default and hidden groups are already included!
+$ExcludeGroup1 = 'WinPE x64'
+$ExcludeGroup2 = 'Windows 10 x64'
+#$ExcludeGroup3 = 'Add more groups here if you don't want certain top level folders sorted.'
+
+#Pull info from XML File
+$XMLGroups = $xml.FirstChild.group
+
+#Builds two separate lists.  One you do not want to edit, and one you do.
+Foreach ($group in $XMLGroups){
+
+    #Separates list into two different groups as you don't want to sort the high level and read-only groups
+    If (
+    $Group.Name -eq 'default' -or
+    $Group.Name -eq 'hidden' -or
+    $Group.Name -eq $ExcludeGroup1 -or
+    $Group.Name -eq $ExcludeGroup2 #-or
+    #$Group.Name -eq $ExcludeGroup3 #uncomment this to add another group you explicitly don't want sorted.
+
+    ){#Add into read only list that will remain at the top of the xml
+    $ReadOnlyList.Add($group)   
+    }
+    else{#Put all other groups into a list to be sorted.
+    $GroupList.Add($group)
+    }
+}
+#Add tag with no XML header to the beginning of the file
+"<groups>" | Out-File -Encoding utf8 -FilePath $exportPath -NoNewline
+
+#Sorts remaining groups into alphabetical order
+$Sorter = $GroupList | Sort Name
+
+#Add read-onlys back to the master list
+ForEach ($listItem in $ReadOnlyList){
+$listItem.OuterXML | Out-File -Encoding utf8 -FilePath $exportPath -Append -NoNewline
+}
+#Add the sorted folders back in
+ForEach ($ChangedItem in $sorter){
+$ChangedItem.OuterXML | Out-File -Encoding utf8 -FilePath $exportPath -Append -NoNewline
+}
+#End the file with /groups
+"</groups>" | Out-File -Encoding utf8 -FilePath $exportPath -Append -NoNewline
